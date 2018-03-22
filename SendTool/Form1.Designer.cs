ed43fdc05bad14d7af554471f97039d053c85685
sunbaochao <758956946@qq.com>
2018-03-22 15:02:30 +0800

    init add

---
+++ SendTool/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace SendTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textURL = new System.Windows.Forms.TextBox();
+            this.textFilePath = new System.Windows.Forms.TextBox();
+            this.richTextReq = new System.Windows.Forms.RichTextBox();
+            this.richTextRes = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SendBtn = new System.Windows.Forms.Button();
+            this.textAppKey = new System.Windows.Forms.TextBox();
+            this.textAppSecret = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL:";
+            // 
+            // textURL
+            // 
+            this.textURL.Location = new System.Drawing.Point(164, 93);
+            this.textURL.Name = "textURL";
+            this.textURL.Size = new System.Drawing.Size(509, 21);
+            this.textURL.TabIndex = 1;
+            // 
+            // textFilePath
+            // 
+            this.textFilePath.Location = new System.Drawing.Point(310, 136);
+            this.textFilePath.Name = "textFilePath";
+            this.textFilePath.Size = new System.Drawing.Size(363, 21);
+            this.textFilePath.TabIndex = 2;
+            // 
+            // richTextReq
+            // 
+            this.richTextReq.Location = new System.Drawing.Point(164, 192);
+            this.richTextReq.Name = "richTextReq";
+            this.richTextReq.Size = new System.Drawing.Size(509, 96);
+            this.richTextReq.TabIndex = 3;
+            this.richTextReq.Text = "";
+            // 
+            // richTextRes
+            // 
+            this.richTextRes.Location = new System.Drawing.Point(164, 319);
+            this.richTextRes.Name = "richTextRes";
+            this.richTextRes.Size = new System.Drawing.Size(509, 96);
+            this.richTextRes.TabIndex = 4;
+            this.richTextRes.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(239, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "FilePath:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Request:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(87, 319);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Response:";
+            // 
+            // SendBtn
+            // 
+            this.SendBtn.Location = new System.Drawing.Point(329, 442);
+            this.SendBtn.Name = "SendBtn";
+            this.SendBtn.Size = new System.Drawing.Size(75, 23);
+            this.SendBtn.TabIndex = 8;
+            this.SendBtn.Text = "Send";
+            this.SendBtn.UseVisualStyleBackColor = true;
+            this.SendBtn.Click += new System.EventHandler(this.SendBtn_Click);
+            // 
+            // textAppKey
+            // 
+            this.textAppKey.Location = new System.Drawing.Point(164, 55);
+            this.textAppKey.Name = "textAppKey";
+            this.textAppKey.Size = new System.Drawing.Size(69, 21);
+            this.textAppKey.TabIndex = 9;
+            // 
+            // textAppSecret
+            // 
+            this.textAppSecret.Location = new System.Drawing.Point(310, 55);
+            this.textAppSecret.Name = "textAppSecret";
+            this.textAppSecret.Size = new System.Drawing.Size(363, 21);
+            this.textAppSecret.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(99, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "AppKey:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(239, 58);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "AppSecret:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(164, 22);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "是否签章";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(164, 139);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(72, 16);
+            this.checkBox2.TabIndex = 14;
+            this.checkBox2.Text = "上传附件";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 511);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textAppSecret);
+            this.Controls.Add(this.textAppKey);
+            this.Controls.Add(this.SendBtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextRes);
+            this.Controls.Add(this.richTextReq);
+            this.Controls.Add(this.textFilePath);
+            this.Controls.Add(this.textURL);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SendTool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textURL;
+        private System.Windows.Forms.TextBox textFilePath;
+        private System.Windows.Forms.RichTextBox richTextReq;
+        private System.Windows.Forms.RichTextBox richTextRes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button SendBtn;
+        private System.Windows.Forms.TextBox textAppKey;
+        private System.Windows.Forms.TextBox textAppSecret;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ SendTool/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Windows.Forms;
+
+namespace SendTool
+{
+    public partial class Form1 : Form
+    {
+        #region Varialbes
+        /// <summary>
+        /// 请求的URL
+        /// </summary>
+        private  string URL = string.Empty;
+        /// <summary>
+        /// 系统唯一标识
+        /// </summary>
+        private  string AppKey = string.Empty;
+        /// <summary>
+        /// 加密的盐
+        /// </summary>
+        private  string AppSecret = string.Empty;
+        /// <summary>
+        /// 是否启用签章
+        /// </summary>
+        private bool EnableSignature = false;
+        /// <summary>
+        /// 是否上传文件
+        /// </summary>
+        private bool EnableUploadFile = false;
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.textURL.Text = "http://[IP]:8765/FileTransfer/FileTransfer/UploadFile";
+            this.textFilePath.Text = "上传的文件路径";
+            this.textFilePath.Enabled = false;
+            this.textAppKey.Enabled = false;
+            this.textAppSecret.Enabled = false;
+        }
+
+        #region Events
+        private void SendBtn_Click(object sender, EventArgs e)
+        {
+            URL = textURL.Text;
+            AppKey = textAppKey.Text;
+            AppSecret = textAppSecret.Text;
+            if(EnableUploadFile)
+                richTextRes.Text=UploadFile(textFilePath.Text);
+            else
+            {
+                string requestJson = richTextReq.Text;
+                if (!string.IsNullOrEmpty(requestJson))
+                {
+                    Byte[] bytes = Encoding.UTF8.GetBytes(requestJson);
+                    richTextRes.Text = Post(bytes, "application/json; charset=UTF-8", EnableSignature);
+                }
+                else
+                {
+                    richTextRes.Text = Get("application/json; charset=UTF-8", EnableSignature);
+                }
+            }
+        }
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            EnableUploadFile = checkBox2.Checked;            
+            this.textFilePath.Enabled = EnableUploadFile;
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            EnableSignature = checkBox1.Checked;
+            this.textAppKey.Enabled = EnableSignature;
+            this.textAppSecret.Enabled = EnableSignature;
+        }
+        #endregion
+
+        /// <summary>
+        /// 上传PDF报告
+        /// </summary>
+        /// <param name="filePath">文件路径</param>
+        /// <returns></returns>
+        private string UploadFile(string filePath)
+        {
+            string result = string.Empty;
+            Byte[] bytes = File.ReadAllBytes(filePath);
+            result = Post(bytes, "application/x-www-form-urlencoded", EnableSignature);
+
+            return result;
+        }
+
+        #region Send http request
+        private string Post(Byte[] requestBytes,string contentType,bool enableSignature)
+        {
+            string result = string.Empty;
+
+            HttpWebRequest httpWebRequest = CreateWebRequest(contentType,"POST", requestBytes, enableSignature);
+
+            try
+            {
+                //get Response
+                HttpWebResponse httpWebResponse = httpWebRequest.GetResponse() as HttpWebResponse;
+                if (httpWebResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    using (Stream recieveStream = httpWebResponse.GetResponseStream())
+                    {
+                        StreamReader reader = new StreamReader(recieveStream, Encoding.UTF8);
+                        result = reader.ReadToEnd();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                result = e.ToString();
+            }
+            return result;
+        }
+        private string Get(string contentType, bool enableSignature)
+        {
+            string result = string.Empty;
+
+            HttpWebRequest httpWebRequest = CreateWebRequest(contentType, "GET", null, enableSignature);
+
+            try
+            {
+                //get Response
+                HttpWebResponse httpWebResponse = httpWebRequest.GetResponse() as HttpWebResponse;
+                if (httpWebResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    using (Stream recieveStream = httpWebResponse.GetResponseStream())
+                    {
+                        StreamReader reader = new StreamReader(recieveStream, Encoding.UTF8);
+                        result = reader.ReadToEnd();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                result = e.ToString();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Create HttpWebRequest object
+        /// </summary>
+        /// <param name="enableSignature"></param>
+        /// <returns></returns>
+        private HttpWebRequest CreateWebRequest(string contentType,string method, Byte[] contentBytes, bool enableSignature)
+        {
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(URL);
+            
+            if (enableSignature)
+            {
+                //Add request hearder
+                httpWebRequest.Headers.Add("HiAuthVersion", "1.1");//Add Version
+                httpWebRequest.Headers.Add("HiAuthAppKey", AppKey);//Add AppKey
+                string requestBody = contentBytes != null?PercentEncode(Convert.ToBase64String(contentBytes)):"";
+                string appSignature = CreateDigitalSignature(URL, method, requestBody, AppSecret);
+                httpWebRequest.Headers.Add("HiAuthAppSignature", appSignature);//Add AppSecret
+            }
+
+            httpWebRequest.ContentType = contentType;
+            httpWebRequest.ContentLength = contentBytes != null ? contentBytes.Length : 0;
+            httpWebRequest.Method = method;
+            httpWebRequest.KeepAlive = true;            
+
+            //add resquest data
+            if(contentBytes !=null)
+                using (Stream stream = httpWebRequest.GetRequestStream())
+                {
+                    stream.Write(contentBytes, 0, contentBytes.Length);
+                    stream.Flush();
+                }
+
+            return httpWebRequest;
+        }
+        #endregion
+
+        #region 生成签章方法
+        private string PercentEncode(string s)
+        {
+            string t = HttpUtility.UrlEncode(s);
+            t = t.Replace("+", "%20");
+            t = t.Replace("!", "%21");
+            t = t.Replace("(", "%28");
+            t = t.Replace(")", "%29");
+            t = t.Replace("*", "%2a");
+            return t;
+        }
+        /// <summary>
+        /// 生成数字签章
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="method"></param>
+        /// <param name="requestBody"></param>
+        /// <param name="secret">加密的salt</param>
+        /// <returns></returns>
+        private string CreateDigitalSignature(string url, string method, string requestBody, string secret)
+        {
+            List<string> combined = new List<string>();
+
+            // request method
+            combined.Add(method.ToUpper());
+
+            Uri uri = new Uri(url);
+            // scheme
+            combined.Add(uri.Scheme.ToLower());
+            // host
+            combined.Add(uri.Host.ToLower());
+            // port
+            combined.Add(uri.Port.ToString());
+            // path
+            string path = uri.AbsolutePath.ToLower();
+            path = path.Replace("\\", "/");
+            if (path.EndsWith("/"))
+                path = path.Substring(0, path.Length - 1);
+            combined.Add(PercentEncode(path));
+
+            // query string
+            string q = (uri.Query ?? "").Trim();
+            if (q.Length > 0)
+            {
+                if (q.StartsWith("?"))
+                    q = q.Substring(1);
+                string[] itemStrs = q.Split(new char[] { '&' }, StringSplitOptions.RemoveEmptyEntries);
+                List<KeyValuePair<string, string>> items = new List<KeyValuePair<string, string>>();
+                foreach (string itemStr in itemStrs)
+                {
+                    if (itemStr.Trim().Length == 0) continue;
+                    string key = "", value = "";
+
+                    int index = itemStr.IndexOf("=");
+                    if (index <= 0) // = is missing or key is missing, ignore
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        key = HttpUtility.UrlDecode(itemStr.Substring(0, index)).Trim().ToLower();
+                        value = HttpUtility.UrlDecode(itemStr.Substring(index + 1)).Trim();
+                        items.Add(new KeyValuePair<string, string>(key, value));
+                    }
+                }
+
+                // query
+                List<string> paramArray = new List<string>();
+                foreach (KeyValuePair<string, string> item in items)
+                {
+                    paramArray.Add(string.Format("{0}={1}", PercentEncode(item.Key), PercentEncode(item.Value)));
+                }
+                combined.Add(String.Join("&", paramArray.ToArray()));
+            }
+            else
+                combined.Add("");
+
+            // body
+            combined.Add(PercentEncode(requestBody ?? ""));
+            // salt
+            combined.Add(secret);
+
+            string baseString = String.Join("|", combined.ToArray());
+
+            System.Security.Cryptography.SHA256Managed s256 = new System.Security.Cryptography.SHA256Managed();
+            byte[] buff;
+            buff = s256.ComputeHash(Encoding.UTF8.GetBytes(baseString));
+            s256.Clear();
+            return Convert.ToBase64String(buff);
+        }
+
+        #endregion
+
+    }
+}
